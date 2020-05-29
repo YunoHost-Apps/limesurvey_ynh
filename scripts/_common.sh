e9da3f224c954936b33812177b0b93684d3839f1
ljf (zamentur) <zamentur@users.noreply.github.com>
2020-05-29 20:41:09 +0200

    [fix] Export in xlsx

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 # App package root directory should be the parent folder
 PKG_DIR=$(cd ../; pwd)
 
-pkg_dependencies="php5-cli php5-imap python-pip php5-gd php5-ldap"
+pkg_dependencies="php7.0-cli php7.05-imap python-pip php7.0-gd php7.0-ldap php7.0-zip"
 
 #=================================================
 # SPECIFIC HELPERS
